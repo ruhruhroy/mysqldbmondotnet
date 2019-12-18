28826ac5318fbc5aa244aa8fbd82d47a8a5268ca
ruhruhroy@gmail.com <mcdoodle22>
2019-12-18 07:55:37 -0500

    Initial version

---
+++ mysqldbmonitordotnet/Controllers/EnvironmentsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using mysqldbmonitordotnet.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace mysqldbmonitordotnet.Controllers
+{
+    public class EnvironmentsController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public EnvironmentsController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
